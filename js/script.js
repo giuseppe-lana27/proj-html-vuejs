05096ad9b8bdb89d76c5255572e5de98a593afc6
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-04-19 12:37:32 +0200

    inserito header e links nel nav

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        navLinks:{
+            home: {
+                name: "Home",
+            },
+            pages: {
+                name: "Pages",
+            },
+            courses: {
+                name: "Courses",
+            },
+            features: {
+                name: "Features",
+            },
+            blog: {
+                name: "Blog",
+            },
+            shop: {
+                name: "Shop",
+            },
+        }
+    }
+});
